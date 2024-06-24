156664dbc4f20ccd55e7eb444bff67555fd749b8
yassineroques <yassine.roques63@gmail.com>
2024-06-24 21:02:42 +0200

    express 01.2 bis

--- src/controllers/movieControllers.js
+++ src/controllers/movieControllers.js
@@ -27,19 +27,33 @@ const movies = [
 
 const database = require("../../database");
 
-
 const getMovies = (req, res) => {
+  let sql = "select * from movies";
+  const sqlValues = [];
+
+  if (req.query.color != null) {
+    sql += " where color = ?";
+    sqlValues.push(req.query.color);
+
+    if (req.query.max_duration != null) {
+      sql += " and duration <= ?";
+      sqlValues.push(req.query.max_duration);
+    }
+  } else if (req.query.max_duration != null) {
+    sql += " where duration <= ?";
+    sqlValues.push(req.query.max_duration);
+  }
+
   database
-    .query("select * from movies")
+    .query(sql, sqlValues)
     .then(([movies]) => {
-      res.json(movies); // use res.json instead of console.log
+      res.json(movies);
     })
     .catch((err) => {
       console.error(err);
-      res.sendStatus(500);
+      res.status(500).send("Error retrieving data from database");
     });
 };
-
 
 const getMovieById = (req, res) => {
   const id = parseInt(req.params.id);
@@ -58,7 +72,6 @@ const getMovieById = (req, res) => {
       res.sendStatus(500);
     });
 };
-
 
 module.exports = {
   getMovies,
--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,40 +1,49 @@
 const database = require("../../database");
 
 const getUsers = (req, res) => {
-    database
-      .query("select * from users")
-      .then(([users]) => {
-        res.json(users); // use res.json instead of console.log
-      })
-      .catch((err) => {
-        console.error(err);
+  let sql = "select * from users";
+  const sqlValues = [];
+
+  if (req.query.language != null) {
+    sql += " where language = ?";
+    sqlValues.push(req.query.language);
+  }
+
+  if (req.query.city != null) {
+    sql += " where city = ?";
+    sqlValues.push(req.query.city);
+  }
+
+  database
+    .query(sql, sqlValues)
+    .then(([users]) => {
+      res.json(users); // use res.json instead of console.log
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(200);
+    });
+};
+
+const getUsersById = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("select * from movies where id = ?", [id])
+    .then(([users]) => {
+      if (users[0] != null) {
+        res.json(users[0]);
+      } else {
         res.sendStatus(200);
-      });
-  };
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(404);
+    });
+};
 
-
-  const getUsersById = (req, res) => {
-    const id = parseInt(req.params.id);
-  
-    database
-      .query("select * from movies where id = ?", [id])
-      .then(([users]) => {
-        if (users[0] != null) {
-          res.json(users[0]);
-        } else {
-          res.sendStatus(200);
-        }
-      })
-      .catch((err) => {
-        console.error(err);
-        res.sendStatus(404);
-      });
-  };
-
-
-  module.exports = {
-    getUsers, getUsersById
-    
-  };
-
-
+module.exports = {
+  getUsers,
+  getUsersById,
+};
